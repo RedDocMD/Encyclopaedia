2a96346a92d2469a9803a1ec63aee7cf2eb8e11b
Deep Majumder <deep.majumder2019@gmail.com>
2020-07-18 20:21:53 +0530

    Chnaged the layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'wiki/index'
+  root 'wiki#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
